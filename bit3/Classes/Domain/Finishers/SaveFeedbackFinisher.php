7e1abb4d85a5ac05750ffd57283d6f60293ec734
nostrabramus <abramotesoro@gmail.com>
2025-04-03 09:36:01 +0200

    added finisher to save to database

---
+++ bit3/Classes/Domain/Finishers/SaveFeedbackFinisher.php
@@ -0,0 +1,94 @@
+<?php
+declare(strict_types=1);
+
+/*
+ *
+ * Save Feedback Form data to database
+ *
+ */
+
+namespace Aip\Bit3\Domain\Finishers;
+
+use TYPO3\CMS\Form\Domain\Finishers\AbstractFinisher;
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Context\Context;
+
+/**
+ * Finisher base class.
+ *
+ * Scope: frontend
+ * **This class is meant to be sub classed by developers**
+ */
+class SaveFeedbackFinisher extends AbstractFinisher
+{
+	protected $defaultOptions = [
+		'clarityRatingField' => 'radiobutton-1',
+		'difficultyAreaField' => 'radiobutton-2',
+		'preferredAspectsField' => 'radiobutton-3',
+		'additionalDetailsField' => 'text-1',
+		'pageUrlField' => 'pageurl',
+		'storagePid' => 0,
+	];
+
+	protected function executeInternal()
+	{
+		$clarityRatingField = $this->parseOption('clarityRatingField');
+		$difficultyAreaField = $this->parseOption('difficultyAreaField');
+		$preferredAspectsField = $this->parseOption('preferredAspectsField');
+		$additionalDetailsField = $this->parseOption('additionalDetailsField');
+		$pageUrlField = $this->parseOption('pageUrlField');
+		$storagePid = $this->parseOption('storagePid');
+
+		$formValues = $this->finisherContext->getFormValues();
+		$FeedbackPid = 1746;
+
+		$data = [
+			'pid' => (int)$FeedbackPid,
+			'clarity_rating' => $formValues[$clarityRatingField] ?? null,
+			'difficulty_area' => $this->formatFieldValue($formValues[$difficultyAreaField] ?? null),
+			'preferred_aspects' => $this->formatFieldValue($formValues[$preferredAspectsField] ?? null),
+			'additional_details' => $formValues[$additionalDetailsField] ?? null,
+			'pageurl' => $formValues[$pageUrlField] ?? null,
+			'tstamp' => time(),
+			'crdate' => time(),
+			'cruser_id' => $this->getCurrentUserId(),
+			'sys_language_uid' => 0,
+			'l10n_parent' => 0,
+			'l10n_diffsource' => '',
+		];
+
+		$connection = GeneralUtility::makeInstance(ConnectionPool::class)
+			->getConnectionForTable('tx_bit3_feedback_data');
+
+		$connection->insert(
+			'tx_bit3_feedback_data',
+			$data
+		);
+	}
+
+	/**
+	 * Formatta il valore del campo per il database
+	 *
+	 * @param mixed $value
+	 * @return string|null
+	 */
+	protected function formatFieldValue($value): ?string
+	{
+		if (is_array($value)) {
+			return implode(',', $value);
+		}
+		return $value !== null ? (string)$value : null;
+	}
+
+	protected function getTypoScriptFrontendController()
+	{
+		return $GLOBALS['TSFE'];
+	}
+
+	protected function getCurrentUserId(): int
+	{
+		$context = GeneralUtility::makeInstance(Context::class);
+		return (int)$context->getPropertyFromAspect('backend.user', 'id', 0);
+	}
+}
--- bit3/ext_tables.sql
+++ bit3/ext_tables.sql
@@ -118,3 +118,46 @@ CREATE TABLE tx_bit3_timeline_item (
 CREATE TABLE sys_file_reference (
     layout tinyint(4) DEFAULT '0' NOT NULL
 );
+
+#
+# Table structure for table 'tx_bit3_feedback_data'
+#
+CREATE TABLE tx_bit3_feedback_data (
+   uid int(11) unsigned NOT NULL auto_increment,
+   pid int(11) DEFAULT '0' NOT NULL,
+
+   clarity_rating VARCHAR(255) DEFAULT NULL,
+   difficulty_area VARCHAR(255) DEFAULT NULL,
+   preferred_aspects VARCHAR(255) DEFAULT NULL,
+   additional_details TEXT DEFAULT NULL,
+   pageurl VARCHAR(255) DEFAULT NULL,
+
+   tstamp int(11) unsigned DEFAULT '0' NOT NULL,
+   crdate int(11) unsigned DEFAULT '0' NOT NULL,
+   cruser_id int(11) unsigned DEFAULT '0' NOT NULL,
+   deleted smallint unsigned DEFAULT '0' NOT NULL,
+   hidden smallint unsigned DEFAULT '0' NOT NULL,
+   starttime int(11) unsigned DEFAULT '0' NOT NULL,
+   endtime int(11) unsigned DEFAULT '0' NOT NULL,
+   sorting int(11) DEFAULT '0' NOT NULL,
+
+   sys_language_uid int(11) DEFAULT '0' NOT NULL,
+   l10n_parent int(11) unsigned DEFAULT '0' NOT NULL,
+   l10n_diffsource mediumblob NULL,
+
+   t3ver_oid int(11) unsigned DEFAULT '0' NOT NULL,
+   t3ver_id int(11) unsigned DEFAULT '0' NOT NULL,
+   t3ver_wsid int(11) unsigned DEFAULT '0' NOT NULL,
+   t3ver_label varchar(255) DEFAULT '' NOT NULL,
+   t3ver_state smallint DEFAULT '0' NOT NULL,
+   t3ver_stage int(11) DEFAULT '0' NOT NULL,
+   t3ver_count int(11) unsigned DEFAULT '0' NOT NULL,
+   t3ver_tstamp int(11) unsigned DEFAULT '0' NOT NULL,
+   t3ver_move_id int(11) unsigned DEFAULT '0' NOT NULL,
+   t3_origuid int(11) unsigned DEFAULT '0' NOT NULL,
+
+   PRIMARY KEY (uid),
+   KEY parent (pid),
+   KEY t3ver_oid (t3ver_oid,t3ver_wsid),
+   KEY language (l10n_parent,sys_language_uid)
+);
